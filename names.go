aa0dcaa6cfa1b381fb9b810ba3cbf0228d7b86cf
Zhenkai Zhu <zhenkai@uber.com>
2017-01-10 15:38:14 -0800

    copy over names

--- names.go
+++ names.go
@@ -19,5 +19,65 @@
 // THE SOFTWARE.
 package dosa
 
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	fqnSeparator = "."
+	rootFQN      = FQN("")
+)
+
 // FQN is the fully qualified name for an entity
 type FQN string
+
+// String satisfies fmt.Stringer interface
+func (f FQN) String() string {
+	return string(f)
+}
+
+// IsPrefix checks if this FQN is prefix to the other FQN
+func (f FQN) IsPrefix(other FQN) bool {
+	if f == rootFQN {
+		return true
+	}
+
+	switch {
+	case len(f) > len(other):
+		return false
+	case len(f) == len(other):
+		return f == other
+	}
+
+	// e.g. "foo.bar" is not prefix of "foo.barz", but is prefix to "foo.bar.z"
+	return other[0:len(f)] == f && other[len(f)] == '.'
+}
+
+// Child returns a new child FQN with the given comp at the end
+func (f FQN) Child(comp string) (FQN, error) {
+	return "", nil
+}
+
+func isInvalidFirstRune(r rune) bool {
+	return !((r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') || r == '_')
+}
+
+func isInvalidOtherRune(r rune) bool {
+	return !(r >= '0' && r <= '9') && isInvalidFirstRune(r)
+}
+
+// CheckName ensures input is a valid name
+// Note that this is the same as this regexp: ^[A-Za-z_][A-Za-z0-9_]+$
+// However, this three pass check is about 4x faster than regexp as of go1.7
+func CheckName(key, value string) error {
+	if len(value) == 0 {
+		return errors.Errorf("%s must not be empty", key)
+	} else if strings.IndexFunc(value[:1], isInvalidFirstRune) != -1 {
+		return errors.Errorf("%s must start with [A-Za-z_]. Actual=%s", key, value)
+	} else if strings.IndexFunc(value[1:], isInvalidOtherRune) != -1 {
+		return errors.Errorf("%s must contain only [A-Za-z0-9_], Actual=%s", key, value)
+	}
+	return nil
+}
