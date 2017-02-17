13ab586bfae15899a8cf917b8afd933550fcbd62
junchaowu <junchaow@uber.com>
2017-02-17 13:36:29 -0800

    Add entitydefinition convert helper methods (#52)

--- connectors/yarpc/helpers.go
+++ connectors/yarpc/helpers.go
@@ -18,12 +18,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package connector
+package yarpc
 
 import (
+	"time"
+
 	"github.com/uber-go/dosa"
 	dosarpc "github.com/uber/dosa-idl/.gen/dosa"
-	"time"
 )
 
 // RawValueAsInterface converts a value from the wire to an object implementing the interface
@@ -80,7 +81,7 @@ func RawValueFromInterface(i interface{}) *dosarpc.RawValue {
 	panic("bad type")
 }
 
-// RPCTypeFromClientType returns the RPC ElemType from a dosa Type
+// RPCTypeFromClientType returns the RPC ElemType from a DOSA Type
 func RPCTypeFromClientType(t dosa.Type) dosarpc.ElemType {
 	switch t {
 	case dosa.Bool:
@@ -101,4 +102,77 @@ func RPCTypeFromClientType(t dosa.Type) dosarpc.ElemType {
 		return dosarpc.ElemTypeUUID
 	}
 	panic("bad type")
+}
+
+// RPCTypeToClientType returns the DOSA Type from RPC ElemType
+func RPCTypeToClientType(t dosarpc.ElemType) dosa.Type {
+	switch t {
+	case dosarpc.ElemTypeBool:
+		return dosa.Bool
+	case dosarpc.ElemTypeBlob:
+		return dosa.Blob
+	case dosarpc.ElemTypeString:
+		return dosa.String
+	case dosarpc.ElemTypeInt32:
+		return dosa.Int32
+	case dosarpc.ElemTypeInt64:
+		return dosa.Int64
+	case dosarpc.ElemTypeDouble:
+		return dosa.Double
+	case dosarpc.ElemTypeTimestamp:
+		return dosa.Timestamp
+	case dosarpc.ElemTypeUUID:
+		return dosa.TUUID
+	}
+	panic("bad type")
+}
+
+// EntityDefinitionToThrift converts the client EntityDefinition to the RPC EntityDefinition
+func EntityDefinitionToThrift(ed *dosa.EntityDefinition) *dosarpc.EntityDefinition {
+	ck := make([]*dosarpc.ClusteringKey, len(ed.Key.ClusteringKeys))
+	for ckinx, clusteringKey := range ed.Key.ClusteringKeys {
+		// TODO: The client uses 'descending' but the RPC uses 'ascending'? Fix this insanity!
+		ascending := !clusteringKey.Descending
+		name := clusteringKey.Name
+		ck[ckinx] = &dosarpc.ClusteringKey{Name: &name, Asc: &ascending}
+	}
+	pk := dosarpc.PrimaryKey{PartitionKeys: ed.Key.PartitionKeys, ClusteringKeys: ck}
+	fd := make(map[string]*dosarpc.FieldDesc, len(ed.Columns))
+	for _, column := range ed.Columns {
+		rpcType := RPCTypeFromClientType(column.Type)
+		fd[column.Name] = &dosarpc.FieldDesc{Type: &rpcType}
+	}
+	fqn := dosarpc.FQN(ed.Name)
+	return &dosarpc.EntityDefinition{PrimaryKey: &pk, FieldDescs: fd, Fqn: &fqn}
+}
+
+// FromThriftToEntityDefinition converts the RPC EntityDefinition to client EntityDefinition
+func FromThriftToEntityDefinition(ed *dosarpc.EntityDefinition) *dosa.EntityDefinition {
+	fields := make([]*dosa.ColumnDefinition, len(ed.FieldDescs))
+	i := 0
+	for k, v := range ed.FieldDescs {
+		fields[i] = &dosa.ColumnDefinition{
+			Name: k,
+			Type: RPCTypeToClientType(*v.Type),
+			// TODO Tag
+		}
+		i++
+	}
+	pk := ed.PrimaryKey.PartitionKeys
+	ck := make([]*dosa.ClusteringKey, len(ed.PrimaryKey.ClusteringKeys))
+	for i, v := range ed.PrimaryKey.ClusteringKeys {
+		ck[i] = &dosa.ClusteringKey{
+			Name:       *v.Name,
+			Descending: !*v.Asc,
+		}
+	}
+
+	return &dosa.EntityDefinition{
+		Name:    ed.Fqn.String(),
+		Columns: fields,
+		Key: &dosa.PrimaryKey{
+			PartitionKeys:  pk,
+			ClusteringKeys: ck,
+		},
+	}
 }
