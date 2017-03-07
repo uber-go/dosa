877ddfcca29ef9f62a247590c535f36336eb6196
Ron Kuris <swcafe@gmail.com>
2017-03-06 17:36:37 -0800

    Add random connector (#78)
    The random connector produces random data on read,
    and works like the devnull connector for writes

--- connectors/random/random_test.go
+++ connectors/random/random_test.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package devnull_test
+package random_test
 
 import (
 	"context"
@@ -26,119 +26,146 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/uber-go/dosa"
-	"github.com/uber-go/dosa/connectors/devnull"
+	"github.com/uber-go/dosa/connectors/random"
+	"time"
 )
 
-var sut = devnull.Connector{}
+var sut = random.Connector{}
+
+type AllTypes struct {
+	dosa.Entity `dosa:"primaryKey=BoolType"`
+	BoolType    bool
+	Int32Type   int32
+	Int64Type   int64
+	DoubleType  float64
+	StringType  string
+	BlobType    []byte
+	TimeType    time.Time
+	UUIDType    dosa.UUID
+}
 
 var (
-	testInfo = &dosa.EntityInfo{
+	testTable, _ = dosa.TableFromInstance((*AllTypes)(nil))
+	testInfo     = &dosa.EntityInfo{
+		Def: &testTable.EntityDefinition,
 		Ref: &dosa.SchemaRef{
 			Scope:      "testScope",
 			NamePrefix: "testPrefix",
 			EntityName: "testEntityName",
 		},
-		Def: &dosa.EntityDefinition{},
 	}
 	testPairs       = dosa.FieldNameValuePair{}
 	testValues      = make(map[string]dosa.FieldValue)
-	testMultiValues = make([]map[string]dosa.FieldValue, 1)
+	testMultiValues = make([]map[string]dosa.FieldValue, 50)
+	fieldsToRead    = []string{"booltype", "int32type", "int64type", "doubletype", "stringtype", "blobtype", "timetype", "uuidtype"}
 )
 
-func TestDevNull_CreateIfNotExists(t *testing.T) {
+func TestRandom_CreateIfNotExists(t *testing.T) {
 	assert.NoError(t, sut.CreateIfNotExists(context.TODO(), testInfo, testValues))
 }
 
-func TestDevNull_Read(t *testing.T) {
-	fieldsToRead := make([]string, 1)
+func TestRandom_Read(t *testing.T) {
 	val, err := sut.Read(context.TODO(), testInfo, testValues, fieldsToRead)
-	assert.Nil(t, val)
-	assert.Error(t, err)
+	assert.NoError(t, err)
+	assert.NotNil(t, val)
+	for _, field := range fieldsToRead {
+		assert.NotNil(t, val[field])
+	}
 }
 
-func TestDevNull_MultiRead(t *testing.T) {
-	fieldsToRead := make([]string, 1)
+func TestRandom_MultiRead(t *testing.T) {
 	v, e := sut.MultiRead(context.TODO(), testInfo, testMultiValues, fieldsToRead)
 	assert.NotNil(t, v)
 	assert.Nil(t, e)
+	assert.Equal(t, len(testMultiValues), len(v))
+	for i := range v {
+		for _, field := range fieldsToRead {
+			assert.NotNil(t, v[i].Values[field])
+		}
+	}
 }
 
-func TestDevNull_Upsert(t *testing.T) {
+func TestRandom_Upsert(t *testing.T) {
 	err := sut.Upsert(context.TODO(), testInfo, testValues)
 	assert.Nil(t, err)
 }
 
-func TestDevNull_MultiUpsert(t *testing.T) {
+func TestRandom_MultiUpsert(t *testing.T) {
 	errs, err := sut.MultiUpsert(context.TODO(), testInfo, testMultiValues)
 	assert.NotNil(t, errs)
 	assert.Nil(t, err)
 }
 
-func TestDevNull_Remove(t *testing.T) {
+func TestRandom_Remove(t *testing.T) {
 	err := sut.Remove(context.TODO(), testInfo, testValues)
 	assert.Error(t, err)
 }
 
-func TestDevNull_MultiRemove(t *testing.T) {
+func TestRandom_MultiRemove(t *testing.T) {
 	errs, err := sut.MultiRemove(context.TODO(), testInfo, testMultiValues)
 	assert.NotNil(t, errs)
 	assert.Nil(t, err)
 }
 
-func TestDevNull_Range(t *testing.T) {
+func TestRandom_Range(t *testing.T) {
 	conditions := make(map[string][]*dosa.Condition)
-	fieldsToRead := make([]string, 1)
-	vals, _, err := sut.Range(context.TODO(), testInfo, conditions, fieldsToRead, "", 0)
-	assert.Nil(t, vals)
-	assert.Error(t, err)
+	vals, _, err := sut.Range(context.TODO(), testInfo, conditions, fieldsToRead, "", 32)
+	assert.NotNil(t, vals)
+	assert.NoError(t, err)
 }
 
-func TestDevNull_Search(t *testing.T) {
-	fieldsToRead := make([]string, 1)
-	vals, _, err := sut.Search(context.TODO(), testInfo, testPairs, fieldsToRead, "", 0)
-	assert.Nil(t, vals)
-	assert.Error(t, err)
+func TestRandom_Search(t *testing.T) {
+	vals, _, err := sut.Search(context.TODO(), testInfo, testPairs, fieldsToRead, "", 32)
+	assert.NotNil(t, vals)
+	assert.NoError(t, err)
 }
 
-func TestDevNull_Scan(t *testing.T) {
-	fieldsToRead := make([]string, 1)
-	vals, _, err := sut.Scan(context.TODO(), testInfo, fieldsToRead, "", 0)
-	assert.Nil(t, vals)
-	assert.Error(t, err)
+func TestRandom_Scan(t *testing.T) {
+	vals, _, err := sut.Scan(context.TODO(), testInfo, fieldsToRead, "", 32)
+	assert.NotNil(t, vals)
+	assert.NoError(t, err)
 }
 
-func TestDevNull_CheckSchema(t *testing.T) {
+func TestRandom_CheckSchema(t *testing.T) {
 	defs := make([]*dosa.EntityDefinition, 4)
 	versions, err := sut.CheckSchema(context.TODO(), "testScope", "testPrefix", defs)
 	assert.NotNil(t, versions)
 	assert.NoError(t, err)
 }
 
-func TestDevNull_UpsertSchema(t *testing.T) {
+func TestRandom_UpsertSchema(t *testing.T) {
 	defs := make([]*dosa.EntityDefinition, 4)
 	versions, err := sut.UpsertSchema(context.TODO(), "testScope", "testPrefix", defs)
 	assert.NotNil(t, versions)
 	assert.NoError(t, err)
 }
 
-func TestDevNull_CreateScope(t *testing.T) {
+func TestRandom_CreateScope(t *testing.T) {
 	assert.NoError(t, sut.CreateScope(context.TODO(), ""))
 }
 
-func TestDevNull_TruncateScope(t *testing.T) {
+func TestRandom_TruncateScope(t *testing.T) {
 	assert.NoError(t, sut.TruncateScope(context.TODO(), ""))
 }
 
-func TestDevNull_DropScope(t *testing.T) {
+func TestRandom_DropScope(t *testing.T) {
 	assert.NoError(t, sut.DropScope(context.TODO(), ""))
 }
 
-func TestDevNull_ScopeExists(t *testing.T) {
-	e, err := sut.ScopeExists(context.TODO(), "")
+func TestRandom_ScopeExists(t *testing.T) {
+	exists, err := sut.ScopeExists(context.TODO(), "")
 	assert.NoError(t, err)
-	assert.True(t, e)
+	assert.True(t, exists)
 }
 
-func TestDevNull_Shutdown(t *testing.T) {
+func TestRandom_Shutdown(t *testing.T) {
 	assert.Nil(t, sut.Shutdown())
+}
+
+// this test is primarily just for 100% coverage
+func TestRandom_badTypePanic(t *testing.T) {
+	testInfo.Def.Columns[0].Type = dosa.Invalid
+	assert.Panics(t, func() {
+		random.Data(testInfo, fieldsToRead)
+	})
 }
