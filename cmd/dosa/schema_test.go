55d9fd5eb1f16e29cd5b02f12705d66f2f8bbdbb
Evan Culver <eculver@users.noreply.github.com>
2017-06-19 13:53:41 -0700

    Validate caller and scope values before invoking YARPC dispatcher (#169)

--- cmd/dosa/schema_test.go
+++ cmd/dosa/schema_test.go
@@ -35,6 +35,16 @@ import (
 	_ "github.com/uber-go/dosa/connectors/devnull"
 )
 
+func TestScopeFlag_String(t *testing.T) {
+	f := scopeFlag("")
+	f.setString("foo.bar.baz")
+	assert.Equal(t, "foo_bar_baz", f.String())
+
+	err := f.UnmarshalFlag("qux.quux.corge")
+	assert.NoError(t, err)
+	assert.Equal(t, "qux_quux_corge", f.String())
+}
+
 func TestSchema_ExpandDirectories(t *testing.T) {
 	assert := assert.New(t)
 	const tmpdir = ".testexpanddirectories"
