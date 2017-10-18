981dd6b9dc00b8d33c3511b29d176afc951ce9d8
Tiffany Cheng <tiffanyc@uber.com>
2017-10-17 17:03:33 -0700

    add exported function documentation

--- connectors/cache/encoding.go
+++ connectors/cache/encoding.go
@@ -19,10 +19,12 @@ func NewJSONEncoder() Encoder {
 
 type jsonEncoder struct{}
 
+// Encode marshals an object using golang's encoding/json package
 func (j *jsonEncoder) Encode(v interface{}) ([]byte, error) {
 	return json.Marshal(v)
 }
 
+// Decode unmarhsals bytes using golagn's encoding/json package
 func (j *jsonEncoder) Decode(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
@@ -34,6 +36,7 @@ func NewGobEncoder() Encoder {
 
 type gobEncoder struct{}
 
+// Encode returns a series of bytes using golang's encoding/gob package
 func (g *gobEncoder) Encode(v interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 	e := gob.NewEncoder(&buf)
@@ -41,6 +44,7 @@ func (g *gobEncoder) Encode(v interface{}) ([]byte, error) {
 	return buf.Bytes(), err
 }
 
+// Decode unmarshals bytes into an object using golang's encoding/gob package
 func (g *gobEncoder) Decode(data []byte, v interface{}) error {
 	e := gob.NewDecoder(bytes.NewBuffer(data))
 	return e.Decode(v)
