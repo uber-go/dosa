1e446d37ee71b9073f01636b039635e3370d2409
tiffanycheng <tiffanycheng91@gmail.com>
2017-10-24 11:14:18 -0700

    Add hit rate stats to redis connector (#234)
    Declare a metrics interface that reports counts. The redis connector uses the counter to report stats on hit and miss rate as well as error counts.

---
+++ metrics/metrics.go
@@ -0,0 +1,20 @@
+package metrics
+
+// Scope is a namespace wrapper around a stats reporter, ensuring that
+// all emitted values have a given prefix or set of tags.
+type Scope interface {
+	// Counter returns the Counter object corresponding to the name.
+	Counter(name string) Counter
+
+	// Tagged returns a new child scope with the given tags and current tags.
+	Tagged(tags map[string]string) Scope
+
+	// SubScope returns a new child scope appending a further name prefix.
+	SubScope(name string) Scope
+}
+
+// Counter is the interface for emitting counter type metrics.
+type Counter interface {
+	// Inc increments the counter by a delta.
+	Inc(delta int64)
+}
