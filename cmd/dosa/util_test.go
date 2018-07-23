5044b73fdc2e730e33198d6534614b7adffc972c
Jian Li <jian.li@uber.com>
2018-07-23 15:10:41 -0700

    Add unit test

--- cmd/dosa/util_test.go
+++ cmd/dosa/util_test.go
@@ -82,6 +82,23 @@ func TestSetQueryFieldValues(t *testing.T) {
 	assert.Contains(t, err.Error(), "is not a valid field for")
 }
 
+func TestPrintResults(t *testing.T) {
+	results := []map[string]dosa.FieldValue{
+		{
+			"id":   dosa.FieldValue(int32(1)),
+			"uuid": dosa.FieldValue(dosa.UUID("3e4befa0-69d3-11e8-95b0-d55aa227a290")),
+		},
+	}
+	err := printResults(results)
+	assert.NoError(t, err)
+}
+
+func TestPrintResultsEmpty(t *testing.T) {
+	results := []map[string]dosa.FieldValue{}
+	err := printResults(results)
+	assert.Error(t, err)
+}
+
 func TestStrToFieldValue(t *testing.T) {
 	tStr := "2018-06-11T13:03:01Z"
 	ts, _ := time.Parse(time.RFC3339, tStr)
